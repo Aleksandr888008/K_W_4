cd1aa4cafa4eeef0b4ee268010a8c5798b577cd1
Александр <learningenglish2804@gmail.com>
2023-06-17 18:11:47 +0400

    add work_v, file_manager

---
+++ src/file_manager.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+import json
+import os
+
+
+class FileManager:
+    """Класс для работы с файлами"""
+
+    def __init__(self, filename):
+        """Сохраняет имя файла, с которым выполняется работа"""
+        self.filename = filename
+
+    @staticmethod
+    def create(filename) -> None:
+        """Проверяет существование файла. Если его нет, то создается пустой файл"""
+
+        if not os.path.exists(os.path.dirname(filename)):
+            os.mkdir(os.path.dirname(filename))
+
+        if not os.path.exists(filename):
+            with open(filename, 'w') as file:
+                file.write(json.dumps([]))
+
+    @staticmethod
+    def read_file(filename) -> list:
+        """Читает данные из файла filename в формате Json"""
+
+        with open(filename, 'r', encoding="utf-8") as file:
+            return json.load(file)
+
+
--- src/workAPI.py
+++ src/workAPI.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 from abc import ABC, abstractmethod
 import requests
 from file_manager import FileManager
-import os
+
 
 urlHH = "https://api.hh.ru/vacancies"
 
@@ -10,7 +11,7 @@ key = "v3.r.137616592.aa33c9bf0b28400429de1f2c8dc68679791e09f9.e72e1f56545343820
 
 
 class API(ABC):
-    """ Абстрактный класс для работы с API по вакансиям"""
+    """ РђР±СЃС‚СЂР°РєС‚РЅС‹Р№ РєР»Р°СЃСЃ РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ API РїРѕ РІР°РєР°РЅСЃРёСЏРј"""
 
     @abstractmethod
     def get_from_api(self):
@@ -18,16 +19,18 @@ class API(ABC):
 
     @staticmethod
     def get_file_manager(filename):
-        """Возвращает объект класса FileManager для работы с файлом"""
+        """Р’РѕР·РІСЂР°С‰Р°РµС‚ РѕР±СЉРµРєС‚ РєР»Р°СЃСЃР° FileManager РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ С„Р°Р№Р»РѕРј"""
         return FileManager(filename)
 
 
 class HeadHunterAPI(API):
+    """РљР»Р°СЃСЃ РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ HeadHunter"""
+
     def __init__(self, profession, page):
         self.url = urlHH
         self.params = {
-            "text": profession,  #запрос профессии
-            "page": page  #страница
+            "text": profession,  #Р·Р°РїСЂРѕСЃ РїСЂРѕС„РµСЃСЃРёРё
+            "page": page  #СЃС‚СЂР°РЅРёС†Р°
         }
 
     def get_from_api(self):
@@ -36,13 +39,17 @@ class HeadHunterAPI(API):
 
 
 class SuperJobAPI(API):
+    """РљР»Р°СЃСЃ РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ Super Job"""
+
     def __init__(self, profession, page):
         self.url = urlSJ
         self.params = {
-            "text": profession,  #запрос профессии
-            "page": page  #страница
+            "text": profession,  #Р·Р°РїСЂРѕСЃ РїСЂРѕС„РµСЃСЃРёРё
+            "page": page  #СЃС‚СЂР°РЅРёС†Р°
         }
 
     def get_from_api(self):
-        response = requests.get(self.url, params=self.params)
+        headers = {
+            "X-Api-App-Id": "v3.r.137616592.aa33c9bf0b28400429de1f2c8dc68679791e09f9.e72e1f56545343820a961593bb2b906754e9370a"}
+        response = requests.get(self.url, headers=headers, params=self.params)
         return response
